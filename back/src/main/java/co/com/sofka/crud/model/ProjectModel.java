9689392ace1471192e9fc5455780d2db7d81ad02
Felipe <fquiceno1@misena.edu.co>
2022-02-27 19:11:57 -0500

    se le da funcionalidad a la clase ProjectModel

--- back/src/main/java/co/com/sofka/crud/model/ProjectModel.java
+++ back/src/main/java/co/com/sofka/crud/model/ProjectModel.java
@@ -1,4 +1,30 @@
 package co.com.sofka.crud.model;
 
+import co.com.sofka.crud.entity.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ProjectModel {
+
+    @Autowired
+    private Project project;
+
+    public Long getId() {
+
+        return project.getId();
+    }
+
+    public void setId(Long id) {
+
+        project.setId(id);
+    }
+
+    public String getName() {
+
+        return project.getName();
+    }
+
+    public void setName(String name) {
+
+        project.setName(name);
+    }
 }
